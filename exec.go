3f041246447e7d99c5919efd94def5d7c38ec5cc
Fred Baube <fbaube@iki.fi>
2024-04-06 18:09:40 +0300

    Refactor into exec/ func

--- exec.go
+++ exec.go
@@ -1,12 +1,10 @@
 package m5cli
 
 import (
-	// "errors"
 	"encoding/json"
 	"fmt"
 	"io"
 	"os"
-	"time"
 
 	DRS "github.com/fbaube/datarepo/sqlite"
 	"github.com/fbaube/m5cli/exec"
@@ -16,8 +14,6 @@ import (
 
 	// mime "github.com/fbaube/fileutils/contentmime"
 	// "github.com/fbaube/tags"
-
-	DRM "github.com/fbaube/datarepo/rowmodels"
 )
 
 // The general approach (semi-OBS):
@@ -411,118 +407,46 @@ func (env *XmlAppEnv) Exec() error {
 	//      See also above
 	// ===============================
 
-	var haveDB bool = (env.SimpleRepo != nil)
-	L.L.Dbg("env.SimpleRepo: <%T> %#v", env.SimpleRepo, env.SimpleRepo)
-	// var jout []byte
-	// var jerr error
-	jout, jerr = json.MarshalIndent(env.SimpleRepo, "SimpleRepo: ", "  ")
-	if jerr != nil {
-		println(jerr)
-		panic(jerr)
-	}
-	L.L.Info("JSON! " + string(jout))
-	println("JSON! " + string(jout))
-
-	var pSR *DRS.SqliteRepo
-	var ok bool
-	pSR, ok = env.SimpleRepo.(*DRS.SqliteRepo)
-	if !ok {
-		panic("Exec: repo is not *SimpleSqliteRepo")
-	}
-	if pSR == nil {
-		L.L.Error("Cannot proceed: SqliteRepo is not valid")
-		os.Exit(1)
-	}
-	// if haveDB && env.cfg.b.DBdoImport {
 	if env.cfg.b.DBdoImport {
-	   	var batchIndex int
-		if !haveDB {
-			panic("Exec: doImport but not haveDB")
-		}
-		var err error
-		L.L.Progress("Will loadicontent for import batch, ID:%d",
-			batchIndex)
-		// var pTx *sql.Tx
-		// =====================
-		//  START A TRANSACTION
-		// =====================
-		err = env.SimpleRepo.Begin()
-		if err != nil {
-			L.L.Error("cli.exec.BeginTx: %w", err)
-		}
-		L.L.Info("TRANSACTION IS STARTED")
-		var timeNow = time.Now().UTC().Format(time.RFC3339)
-		// ============================
-		//  FOR EVERY Input Contentity
-		// ============================
-		for _, pMCF := range InputContentities {
-			// Prepare a DB record for the File
-			pMCF.T_Imp = timeNow
-			// L.L.Info("exec.L463: Trying new INSERT Generic")
-			/*
-				var stmt string
-				// OBS stmt, e = pSR.NewInsertStmt(&pMCF.ContentityRow)
-				stmt, e = DRS.NewInsertStmtGnrcFunc(
-				      pSR, &pMCF.ContentityRow)
-				if e != nil {
-					return mcfile.WrapAsContentityError(e,
-					  "new insert contentity stmt (cli.exec)", pMCF)
-				}
-				var insID int
-				insID, e = pSR.ExecInsertStmt(stmt)
-			*/
-			var insID int
-			insID, e = DRS.DoInsertGeneric(pSR, &pMCF.ContentityRow)
-			if e != nil {
-				return mcfile.WrapAsContentityError(e,
-					"insert contentity to DB (cli.exec)", pMCF)
-			}
-			L.L.Info("Added file to import batch, ID: %d", insID)
-		}
-		pIB := new(DRM.InbatchRow)
-		pIB.FilCt = len(InputContentities)
-		pIB.Descr = "CLI import"
-		// pIB.RelFP =
-		// pIB.AnsFP =
-		pIB.T_Cre, pIB.T_Imp = timeNow, timeNow
-		/*
-			var stmt string
-			stmt, e = pSR.NewInsertStmt(pIB)
-			if e != nil {
-				return fmt.Errorf("new insert inbatch stmt (cli.exec): %w", e)
-			}
-			var insID int
-			insID, e = pSR.ExecInsertStmt(stmt)
-		*/
-		var insID int
-		insID, e = DRS.DoInsertGeneric(pSR, pIB)
 
-		if e != nil {
-			return fmt.Errorf("new insert inbatch to DB (cli.exec): %w", e)
+		if haveDB := (env.SimpleRepo != nil); !haveDB {
+		   L.L.Error("Cannot proceed: SqliteRepo is not valid")
+		   os.Exit(1)
 		}
-		L.L.Okay("cli/exec: INSERT'd inbatch OK, ID: %d", insID)
-
-		e = pSR.Commit()
-		if e != nil {
-			return mcfile.WrapAsContentityError(e,
-				"commit txn to DB failed (cli.exec)", nil)
+		// ========================
+		//  Verify the type of the
+		//   repo (future-proofing)
+		// ========================
+		var pSR *DRS.SqliteRepo
+		var ok bool
+		pSR, ok = env.SimpleRepo.(*DRS.SqliteRepo)
+		if !ok {
+		   panic("Exec: repo is not *SimpleSqliteRepo")
 		}
-		L.L.Okay("Batch imported OK: TRANSACTION SUCCEEDED")
-		// env.SimpleRepo.Tx = nil
-		// pp := pCA.SimpleRepo.GetFileAll()
-		// fmt.Printf("    DD:Files len %d id[last] %d \n", len(pp), fileIndex)
+		// =============
+		//  Output some
+		//   debug info 
+		// =============
+		L.L.Dbg("env.SimpleRepo: %#v", pSR)
+		jout, jerr = json.MarshalIndent(
+			env.SimpleRepo, "env.SimpleRepo: ", "  ")
+		if jerr != nil {
+		   println(jerr)
+		   panic(jerr)
+		}
+		L.L.Info("JSON! " + string(jout))
+		println("JSON! " + string(jout))
+		// =================
+		//   NOW IT'S OKAY
+		//    TO PROCEED
+		//  WITH THE IMPORT
+		// =================
+		importError := exec.ImportBatchIntoDB(env, InputContentities)
+		if importError != nil {
+		   L.L.Error("exec.ImportBatchIntoDB failed: %w", importError)
+		}
+		pSR.CloseLogWriter()
 	}
-	L.L.Info("TRYING SELECT BY ID")
-	stmtS, eS := pSR.NewSelectByIdStmt(&DRM.TableDetailsCNT, 1)
-	if eS != nil {
-		return fmt.Errorf("new select contentity by id=1 stmt (cli.exec): %w", eS)
-	}
-	result, e3 := DRS.ExecSelectOneStmt[*DRM.ContentityRow](pSR, stmtS)
-	if e3 != nil {
-		return fmt.Errorf("new select contentity by id=1 from DB (cli.exec): %w", e3)
-	}
-	L.L.Warning("exec.go: INSERT'd inbatch OK, ID:%d", result)
-	pSR.CloseLogWriter()
 	return nil
 }
 
--- go.mod
+++ go.mod
@@ -5,12 +5,12 @@ go 1.22
 toolchain go1.22.1
 
 require (
-	github.com/fbaube/datarepo v0.0.0-20240327103543-e67319cad3c8
+	github.com/fbaube/datarepo v0.0.0-20240405095351-74d388b4168b
 	github.com/fbaube/dsmnd v0.0.0-20240130143636-adbc149a7741
 	github.com/fbaube/fileutils v0.0.0-20240326135138-a85c3bc4c0ed
 	github.com/fbaube/logutils v0.0.0-20240118190821-fba0e702dd07
 	github.com/fbaube/lwdx v0.0.0-20240327195153-e4bf7566f0ba
-	github.com/fbaube/mcfile v0.0.0-20240327195229-b053eca48b3b
+	github.com/fbaube/mcfile v0.0.0-20240331170550-39468ce434c9
 	github.com/fbaube/mlog v0.0.0-20230904080411-cfac35fed413
 	github.com/fbaube/stringutils v0.0.0-20240326203406-bf4fec3db0ff
 	github.com/fbaube/wasmutils v0.0.0-20231209183154-e25018375bc2
@@ -22,7 +22,7 @@ require (
 require (
 	github.com/fatih/color v1.16.0 // indirect
 	github.com/fbaube/contentanalysis v0.0.0-20240327145624-e50cebd6ca74 // indirect
-	github.com/fbaube/ctoken v0.0.0-20240327145556-74a636fac33d // indirect
+	github.com/fbaube/ctoken v0.0.0-20240331170316-48bee35bdbb4 // indirect
 	github.com/fbaube/gparse v0.0.0-20230421173125-0aaf74eb920c // indirect
 	github.com/fbaube/gtoken v0.0.0-20240326202203-4d8380e83252 // indirect
 	github.com/fbaube/gtree v0.0.0-20240326202334-f3d75c2d5e8d // indirect
@@ -31,16 +31,16 @@ require (
 	github.com/fbaube/orderednodes v0.0.0-20240327103507-96eed71fc379 // indirect
 	github.com/fbaube/parseutils v0.0.0-20240117103929-681897b1f883 // indirect
 	github.com/gabriel-vasile/mimetype v1.4.3 // indirect
-	github.com/gomarkdown/markdown v0.0.0-20231222211730-1d6d20845b47 // indirect
+	github.com/gomarkdown/markdown v0.0.0-20240328165702-4d01890c35c0 // indirect
 	github.com/lainio/err2 v1.0.0 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/mattn/go-sqlite3 v1.14.22 // indirect
 	github.com/mgutz/str v1.2.0 // indirect
-	github.com/yuin/goldmark v1.7.0 // indirect
-	golang.org/x/exp v0.0.0-20240325151524-a685a6edb6d8 // indirect
-	golang.org/x/net v0.22.0 // indirect
-	golang.org/x/sys v0.18.0 // indirect
-	golang.org/x/tools v0.19.0 // indirect
+	github.com/yuin/goldmark v1.7.1 // indirect
+	golang.org/x/exp v0.0.0-20240404231335-c0f41cb1a7a0 // indirect
+	golang.org/x/net v0.24.0 // indirect
+	golang.org/x/sys v0.19.0 // indirect
+	golang.org/x/tools v0.20.0 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 )
